821a295fd70c14476d1340df51a06f85de4b8e3e
Dialest <dialest@hotmail.com>
2019-04-27 00:31:20 -0700

    Add mongo to Heroku

--- server.js
+++ server.js
@@ -30,8 +30,10 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/siliconscrapera", { useNewUrlParser: true });
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/siliconscrapera";
+
+mongoose.connect(MONGODB_URI);
 
 // Routes
 app.get("/", function(req, res) {
